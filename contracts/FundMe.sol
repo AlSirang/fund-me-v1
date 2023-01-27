3ede27a4979734cc2f4a9b4aeeb551b6d9f3c94d
AlSirang <uddinbashir1999@gmail.com>
2023-01-27 15:12:16 +0500

    fix: complete Fund Me contract

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -1,31 +1,75 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+import "./libs/PriceConverter.sol";
+
 contract FundMe {
-    uint256 public unlockTime;
-    address payable public owner;
+    using PriceConverter for uint256;
 
-    event Withdrawal(uint256 amount, uint256 when);
+    address private _priceFeedContract;
+    address public owner;
+    uint256 public minimumFunds = 50 * 1e18; // convert USD in terms of Wei
 
-    constructor(uint256 _unlockTime) payable {
-        require(
-            block.timestamp < _unlockTime,
-            "Unlock time should be in the future"
-        );
+    address[] public funders;
+    mapping(address => uint256) public fundersToFunds;
 
-        unlockTime = _unlockTime;
-        owner = payable(msg.sender);
+    // modifiers
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Caller is not owner");
+        _;
     }
 
-    function withdraw() public {
-        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+    function fund() external payable {
+        require(
+            msg.value.getConversionRate(_priceFeedContract) >= minimumFunds,
+            "did't send enough funds"
+        );
 
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
+        // if funder already exists in funders list do not add address again
+        if (fundersToFunds[msg.sender] == 0) funders.push(msg.sender);
+        fundersToFunds[msg.sender] += msg.value;
+    }
 
-        emit Withdrawal(address(this).balance, block.timestamp);
+    function withdraw() external onlyOwner {
+        for (uint256 i = 0; i < funders.length; i++) {
+            address funder = funders[i];
+            delete fundersToFunds[funder]; // reset mapping for givein address by deleting it. it will reset value to zero
+            // or
+            fundersToFunds[funder] = 0; // reset value to 0
+        }
 
-        owner.transfer(address(this).balance);
+        funders = new address[](0); // new empty array
+
+        //  funds transfer
+
+        /*
+            // transfer
+            // throws error if transfer failed
+            payable(msg.sender).transfer(address(this).balance);
+
+            // send
+            // returns boolean for send status. true for success false for failure
+            bool success = payable(msg.sender).send(address(this).balance);
+            require(success, "transfer failed");
+        */
+
+        // call
+        // it can be used to call any function without abi. it returns two values. function call stauts (true | false) and data (any data returned from called function)
+
+        // (bool callSuccess, bytes memory dataReturned) = payable(msg.sender).call{value: address(this).balance}("");
+
+        // call is recommended way to send funds
+        (bool callSuccess, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
+
+        require(callSuccess, "transfer failed");
+    }
+
+    constructor(address priceFeedContract_) {
+        // 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e goerli ETH/USD
+        _priceFeedContract = priceFeedContract_;
+        owner = msg.sender;
     }
 }
