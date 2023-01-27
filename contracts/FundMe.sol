017a649c9dcdc36ddb68df21192fd886e07a654d
AlSirang <uddinbashir1999@gmail.com>
2023-01-27 15:35:36 +0500

    fix: gas optimization

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -3,12 +3,21 @@ pragma solidity ^0.8.9;
 
 import "./libs/PriceConverter.sol";
 
+error NotOwner();
+error TransferFailed();
+error LowFunds();
+
 contract FundMe {
     using PriceConverter for uint256;
 
-    address private _priceFeedContract;
-    address public owner;
-    uint256 public minimumFunds = 50 * 1e18; // convert USD in terms of Wei
+    // immutable
+    // storage variables which can be set once
+    address private immutable _i_priceFeedContract;
+    address public immutable i_owner;
+
+    // constant
+    // initialize once while declaring variables
+    uint256 public constant MINIMUM_FUNDS = 50 * 1e18; // convert USD in terms of Wei
 
     address[] public funders;
     mapping(address => uint256) public fundersToFunds;
@@ -16,15 +25,19 @@ contract FundMe {
     // modifiers
 
     modifier onlyOwner() {
-        require(msg.sender == owner, "Caller is not owner");
+        // require(msg.sender == i_owner, "Caller is not owner");
+        if (msg.sender != i_owner) revert NotOwner();
         _;
     }
 
-    function fund() external payable {
-        require(
-            msg.value.getConversionRate(_priceFeedContract) >= minimumFunds,
-            "did't send enough funds"
-        );
+    function fund() public payable {
+        // require(
+        //     msg.value.getConversionRate(_i_priceFeedContract) >= MINIMUM_FUNDS,
+        //     "did't send enough funds"
+        // );
+
+        if (msg.value.getConversionRate(_i_priceFeedContract) < MINIMUM_FUNDS)
+            revert LowFunds();
 
         // if funder already exists in funders list do not add address again
         if (fundersToFunds[msg.sender] == 0) funders.push(msg.sender);
@@ -51,7 +64,9 @@ contract FundMe {
             // send
             // returns boolean for send status. true for success false for failure
             bool success = payable(msg.sender).send(address(this).balance);
-            require(success, "transfer failed");
+            // require(success, "transfer failed");
+            if (!success) revert TransferFailed();
+
         */
 
         // call
@@ -64,12 +79,39 @@ contract FundMe {
             value: address(this).balance
         }("");
 
-        require(callSuccess, "transfer failed");
+        // require(callSuccess, "transfer failed");
+
+        if (!callSuccess) revert TransferFailed();
+    }
+
+    /* Which function is called, fallback() or receive()?
+
+                send Ether
+                    |
+                msg.data is empty?
+                    / \
+                    yes  no
+                    /     \
+        receive() exists?  fallback()
+                /   \
+                yes   no
+                /      \
+            receive()   fallback()
+    */
+
+    // Function to receive Ether. msg.data must be empty
+    receive() external payable {
+        fund();
+    }
+
+    // Fallback function is called when msg.data is not empty
+    fallback() external payable {
+        fund();
     }
 
     constructor(address priceFeedContract_) {
         // 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e goerli ETH/USD
-        _priceFeedContract = priceFeedContract_;
-        owner = msg.sender;
+        _i_priceFeedContract = priceFeedContract_;
+        i_owner = msg.sender;
     }
 }
