9550c0f13ede5f55bc9d07918408ffc171163b1e
AlSirang <uddinbashir1999@gmail.com>
2023-01-27 15:11:56 +0500

    feat: create Price Converter library

--- contracts/libs/PriceConverter.sol
+++ contracts/libs/PriceConverter.sol
@@ -1,4 +1,31 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-library PriceConverter {}
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+library PriceConverter {
+    function getPrice(address _priceFeedContract)
+        internal
+        view
+        returns (uint256)
+    {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            _priceFeedContract
+        );
+        // ETH/USD price feed contract
+
+        (, int256 answer, , , ) = priceFeed.latestRoundData();
+
+        // price has 8 decimals and to make it compatible with eth units it should be in Wei
+        return uint256(answer * 1e10); // convert USD price into Wei.
+    }
+
+    function getConversionRate(uint256 ethAmount, address _priceFeedContract)
+        internal
+        view
+        returns (uint256)
+    {
+        uint256 ethPrice = getPrice(_priceFeedContract); // get latest price of ETH/USD
+        return (ethAmount * ethPrice) / 1e18; // compute ethAmount in terms of USD -> fix decimals
+    }
+}
